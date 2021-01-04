4bee93ea1f6dc59a901f1d97e1da912def07d814
Pedro Victor da Fonseca <peidrao>
2021-01-04 18:43:55 -0300

    add model Ingredient

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 def sample_user(email='test@recipeapi.com', password='@Testpass'):
@@ -53,3 +53,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """ test the ingredient string representation """
+        ingredient = Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
