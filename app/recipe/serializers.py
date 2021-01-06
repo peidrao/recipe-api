010c8a0422bf91703eb445bb04212518a5104c86
Pedro Victor da Fonseca <peidrao>
2021-01-06 11:03:51 -0300

    add recipe image upload field

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -40,3 +40,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """ Serializer a recipe detail """
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerialzier(serializers.ModelSerializer):
+    """ Serializer for uploading images to recipes """
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
